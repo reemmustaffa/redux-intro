f90c53555530a067e046b5ebc1afe1ed6dfd6c6f
reem_mustafa <reemmustafa905@gmail.com>
2025-09-19 23:27:11 +0300

    working with action creator

--- src/store.js
+++ src/store.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { act } from "react";
 import { createStore } from "redux";
 
@@ -36,17 +37,42 @@ function reducer(state = initialState, action) {
 const store = createStore(reducer);
 
 console.log(store);
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+store.dispatch(requestLoan(1000, "Buy cheap car"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
