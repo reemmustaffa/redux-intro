17fcc8c2451f05b632256282863a86850eb193ab
reem_mustafa <reemmustafa905@gmail.com>
2025-09-20 12:11:34 +0300

    connecting our redux app with react

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+  //دي موجوده عشان اشترك في الريدكس واقدر يكون ليا اكسس عليه
+  const customer = useSelector((store) => store.customer);
+
+  console.log(customer);
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
